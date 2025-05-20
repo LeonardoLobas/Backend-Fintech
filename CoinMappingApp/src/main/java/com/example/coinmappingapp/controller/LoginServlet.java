01dd07d6780b822852d35ea804c046276230b72c
LeonardoLobas <leonardolobas2703@gmail.com>
2025-05-20 11:03:05 -0300

    Logica para login utilizando criptografia e organizando estilização pagina de logins e cadastros.

---
+++ CoinMappingApp/src/main/java/com/example/coinmappingapp/controller/LoginServlet.java
@@ -0,0 +1,49 @@
+
+package com.example.coinmappingapp.controller;
+
+import com.example.coinmappingapp.dao.UserDao;
+import com.example.coinmappingapp.factory.DaoFactory;
+import com.example.coinmappingapp.model.User;
+import com.example.coinmappingapp.util.CriptografiaUtils;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private UserDao dao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        dao = DaoFactory.getUserDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  ServletException, IOException {
+        String email = req.getParameter("email");
+        String senha = req.getParameter("password");
+
+        try {
+            String senhaCriptografada = CriptografiaUtils.criptografia(senha);
+
+            User user = dao.buscarPorEmailESenha(email,senhaCriptografada);
+            if (user != null) {
+                // Usuário encontrado, login bem-sucedido
+                req.getSession().setAttribute("usuarioLogado", user);
+                resp.sendRedirect("index.jsp");
+            } else {
+                req.setAttribute("erro", "E-mail ou senha inválidos.");
+                req.getRequestDispatcher("loginUser.jsp").forward(req, resp);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            req.setAttribute("erro", "Erro ao processar Login");
+            req.getRequestDispatcher("loginUser.jsp").forward(req, resp);
+        }
+    }
+}
--- CoinMappingApp/src/main/webapp/header.jsp
+++ CoinMappingApp/src/main/webapp/header.jsp
@@ -1,9 +1,18 @@
+<%@ page session="false" %>
+<%
+  String currentPage = request.getRequestURI();
+  String contextPath = request.getContextPath();
+
+  boolean isLoginPage = currentPage.contains("loginUser.jsp");
+  boolean isCadastroPage = currentPage.contains("cadastroUser.jsp");
+  boolean isRoot = currentPage.equals(contextPath) || currentPage.equals(contextPath + "/");
+%>
+
 <header>
   <nav class="navbar navbar-expand-lg" style="background-color: #008b8b;">
     <div class="container-fluid">
       <a class="navbar-brand text-white" href="./index.jsp">
         <img src="./resources/images/Logo.svg" alt="Logo" class="d-inline-block align-text-top">
-
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
               aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,12 +20,14 @@
       </button>
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
+          <% if (!isLoginPage && !isCadastroPage && !isRoot) { %>
           <li class="nav-item ms-5">
             <a class="nav-link text-white" href="./loginUser.jsp">Login</a>
           </li>
           <li class="nav-item ms-5">
             <a class="btn btn-light" href="./cadastroUser.jsp">Comece a gerenciar</a>
           </li>
+          <% } %>
         </ul>
       </div>
     </div>
